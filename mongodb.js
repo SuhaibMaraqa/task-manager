8968470deb7de508613adad939c818389e43c518
Sting1X <suhaibmaraqaapple@gmail.com>
2022-03-07 22:13:04 +0300

    complete CRUD operations

--- mongodb.js
+++ mongodb.js
@@ -1,38 +1,71 @@
-const mongodb = require('mongodb');
-const mongoClient = mongodb.MongoClient
+// const mongodb = require('mongodb');
+// const mongoClient = mongodb.MongoClient
+
+const { MongoClient, ObjectID } = require('mongodb')
 
 const connectionURL = 'mongodb://127.0.0.1:27017';
 const databaseName = 'task-manager'
 
-mongoClient.connect(connectionURL, {useNewUrlParser: true}, (err, client) => {
+MongoClient.connect(connectionURL, {useNewUrlParser: true}, (err, client) => {
      if (err) {
           return console.log('Unable to connect to database!');
      }
 
      const db = client.db(databaseName)
 
-     // db.collection('users').insertOne({
-     //      name: 'Suhaib',
-     //      age: 21
+     // db.collection('users').findOne({age: 21}, (err, user)=>{
+     //      if (err) {
+     //           return console.log("Unable to fetch");
+     //      }
+     //      return console.log(user)
+     // });
+     
+     // db.collection('users').find({age: 21}).toArray((err, users)=>{
+     //      if (err) {
+     //           return console.log("Unable to fetch")
+     //      }
+     //      return console.log(users)
      // })
 
-     db.collection('tasks').insertMany([
-          {
-               description: 'wake up',
-               completed: true
-          }, {
-               description: 'eat',
-               completed: true
-          }, {
-               description: 'sleep',
-               completed: false
-          }
-     ], (err, result)=>{
-          if (err) {
-               return console.log("Unable to insert to tasks collection");
-          } else {
-               console.log(result);
-          }
-     })
+     // db.collection('tasks').findOne({_id: new ObjectID("6225f6f412e1d539df26ed34")}, (err, task)=>{
+     //      console.log(task);
+     // })
+
+     // db.collection('tasks').find({completed: true}).toArray((err, tasks)=>{
+     //      console.log(tasks);
+     // })
+
+     // db.collection('users').updateOne({
+     //      _id: new ObjectID("6225fbc086f7d17c90bf667b")
+     // }, {
+     //      // $set:{name: "Yahya"}
+     //      $inc:{age: 1}
+     // }).then((result)=>{
+     //      console.log(result);
+     // }).catch((error)=>{
+     //      console.log(error);
+     // })
+
+     // db.collection('tasks').updateMany({completed: false}, {$set:{
+     //      completed: true
+     // }}).then((result)=>{
+     //      console.log(result);
+     // }).catch((error)=>{
+     //      console.log(error);
+     // })
+
+     // db.collection('users')
+     //      .deleteMany({age: {$gt: 25}})
+     //      .then((result)=>{
+     //           console.log(result);
+     //      })
+     //      .catch((error)=>{
+     //           console.log(error);
+     //      })
+
+
+
+
+
      console.log('Connected to database successfully!');
 })
